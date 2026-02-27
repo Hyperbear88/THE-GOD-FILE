abfaefa1f001a7192e691cde5998e0c0d27b9a56
Daniel Semakula <danielsemakula89@gmail.com>
2026-02-27 17:20:44 +0000

    The big change

--- THE GOD FILE - Python Project (Stable)/app.py
+++
@@ -1,4 +0,0 @@
-﻿from REFACTOR.runtime import safe_main
-
-if __name__ == "__main__":
-    safe_main()
--- THE GOD FILE - Python Project (Stable)/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\danie\Desktop\BACKUPs\THE GOD FILE - Python Project
-py -3.13 app.py
-pause
